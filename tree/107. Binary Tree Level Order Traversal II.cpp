7673fa7183c9b3e204e4b46d3e3422c539068b44
singcs14-cloud <sing.cs14@nycu.edu.tw>
2026-08-16 01:37:08 +0800

    Create 107. Binary Tree Level Order Traversal II.cpp

---
+++ tree/107. Binary Tree Level Order Traversal II.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+         vector<vector<int>> ans;
+         preorder( ans , root , 0);
+         reverse(ans.begin(), ans.end());
+         return ans;
+    }
+    void preorder( vector<vector<int>>& ans , TreeNode* cur , int level){
+        if(!cur)return;
+        if(ans.size() < level+1)ans.push_back( vector<int> ());
+
+        ans[level].push_back(cur->val);
+        if(cur->left) preorder( ans , cur->left , level+1);
+        if(cur->right) preorder( ans , cur->right , level+1);
+        return;
+    }
+};
