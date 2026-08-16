42b59c6598dea5260dfd791146d9118b7990aa18
singcs14-cloud <sing.cs14@nycu.edu.tw>
2026-08-16 16:21:13 +0800

    Create 429. N-ary Tree Level Order Traversal.cpp

---
+++ tree/429. N-ary Tree Level Order Traversal.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+       
+        queue<Node*> q;
+        q.push(root);
+        vector<vector<int>> ans;
+
+        if(!root)return ans;
+        
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> res;
+
+            for(int i=0 ; i<size ;i++){
+                Node* node = q.front();
+                q.pop();
+                res.push_back(node->val);
+                for(int j=0;j<node->children.size();j++){
+                    if(node->children[j]) q.push(node->children[j] );
+                }
+            }
+            ans.push_back(res);
+        }
+        return ans;
+    }
+};
